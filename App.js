c0afbfb0abf461cf4fbfca1f2b96c32c63212803
Tamanna <tamannas10110@gmail.com>
2024-01-27 11:44:19 +0530

    episode-03 pushed

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "div", 
-    {id:"parent"}, 
-    React.createElement(
-        "div", 
-        {id:"child"}, 
-        [
-        React.createElement(
-            "h1", 
-            {}, 
-            "Testing child"
-            ),
-        React.createElement(
-            "h2", 
-            {}, 
-            "Testing siblings"
-            )  
-        ]
-        )
-    );
+const heading = <div>I'm Heading element</div>
+
+const HeadingComponent = () => {
+    return (
+        <>
+            <div>I'm Heading component</div>
+            
+        </>
+    )
+}
+
+const MainComponent = () => {
+    return (
+        <>
+        <div>I'm main component</div>
+        <HeadingComponent/>
+        {heading}
+        </>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<MainComponent/>);
